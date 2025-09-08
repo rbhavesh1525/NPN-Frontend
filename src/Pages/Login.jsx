8e75fd64ecefcbec2e54387a5c962f46f5654b9e
Suyash Dashputre <suyashdashputre@gmail.com>
2025-09-08 09:45:56 +0530

    Final frontend

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -115,11 +115,8 @@ export default function LoginPage() {
             </div>
             
             <div className="space-y-2">
-              <div className="flex items-center justify-between">
+              <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
-                <Link to="/pages/forgot-password" className="text-sm font-medium text-blue-600 hover:underline">
-                  Forgot password?
-                </Link>
               </div>
               <Input 
                 id="password" 
